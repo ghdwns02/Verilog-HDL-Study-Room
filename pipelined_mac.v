dd3e466b38f71d742a43adab451007c06f4fb15f
Hongjun Lee <02.ghdwns@gmail.com>
2025-09-04 19:21:48 +0900

    Add files via upload

--- pipelined_mac.v
+++ pipelined_mac.v
@@ -1,28 +1,50 @@
-module pipelined_mac
+//3-Tap FIR filter
+module pipelined_fir
 #(parameter WIDTH = 8)
 (
     input clk,
     input rst,
-    input [WIDTH-1:0] A,
-    input [WIDTH-1:0] B,
-    output reg [WIDTH*2:0] result
+    input [WIDTH-1:0] din,
+    output reg[WIDTH*2+1:0] dout
 );
 
-reg [WIDTH*2-1:0] mul_result_stage1; 
+parameter C0 = 2'd2;
+parameter C1 = 2'd3;
+parameter C2 = 2'd1;
+
+reg [WIDTH-1:0] Xn, Xn_1, Xn_2;
 
 always @ (posedge clk or posedge rst) begin
     if (rst) begin
-        mul_result_stage1 <= 0;
+        Xn <= 0;
+        Xn_1 <= 0;
+        Xn_2 <= 0;
     end else begin
-        mul_result_stage1 <= A * B;
+        Xn <= din;
+        Xn_1 <= Xn;
+        Xn_2 <= Xn_1;
+    end
+end
+
+reg [WIDTH*2-1:0]mul0_reg, mul1_reg, mul2_reg;
+
+always @ (posedge clk or posedge rst) begin
+    if (rst) begin
+        mul0_reg <= 0;
+        mul1_reg <= 0;
+        mul2_reg <= 0;
+    end else begin
+        mul0_reg <= C0 * Xn;
+        mul1_reg <= C1 * Xn_1;
+        mul2_reg <= C2 * Xn_2;
     end
 end
 
 always @ (posedge clk or posedge rst) begin
     if (rst) begin
-        result <= 0;
+        dout <= 0;
     end else begin
-        result <= result + mul_result_stage1;
+        dout <= mul0_reg + mul1_reg + mul2_reg;
     end
 end
 
